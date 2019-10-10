05735176b146d148756b8fc2fa9663662c60672a
Igor Skljar <39621064+it240884sii@users.noreply.github.com>
2019-10-10 17:27:57 +0300

    Update select.groovy

--- src/dist/cfg/groovy/select.groovy
+++ src/dist/cfg/groovy/select.groovy
@@ -15,9 +15,5 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import org.jpos.iso.ISOMsg;
-import org.jpos.transaction.Context;
-import org.jpos.transaction.ContextConstants;
 
 cfg[ctx.REQUEST.MTI]
-
